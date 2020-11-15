6dea5c8f7d998d6907941bf951c09eeece4f3979
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-15 15:32:29 -0800

    Generate IEquatable

--- src/Unterpolate/ParsedCapture.cs
+++ src/Unterpolate/ParsedCapture.cs
@@ -1,9 +1,28 @@
-﻿namespace Knapcode.Unterpolate
+﻿using System;
+using System.Collections.Generic;
+
+namespace Knapcode.Unterpolate
 {
-    internal class ParsedCapture
+    internal class ParsedCapture : IEquatable<ParsedCapture>
     {
         public ParsedCapture(string name) => Name = name;
 
         public string Name { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ParsedCapture);
+        }
+
+        public bool Equals(ParsedCapture other)
+        {
+            return other != null &&
+                   Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return 539060726 + EqualityComparer<string>.Default.GetHashCode(Name);
+        }
     }
 }
--- src/Unterpolate/Token.cs
+++ src/Unterpolate/Token.cs
@@ -1,9 +1,32 @@
-﻿namespace Knapcode.Unterpolate
+﻿using System;
+using System.Collections.Generic;
+
+namespace Knapcode.Unterpolate
 {
-    internal class Token
+    internal class Token : IEquatable<Token>
     {
         public Token(TokenType type, string value) => (Type, Value) = (type, value);
         public TokenType Type { get; }
         public string Value { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Token);
+        }
+
+        public bool Equals(Token other)
+        {
+            return other != null &&
+                   Type == other.Type &&
+                   Value == other.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1265339359;
+            hashCode = hashCode * -1521134295 + Type.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Value);
+            return hashCode;
+        }
     }
 }
